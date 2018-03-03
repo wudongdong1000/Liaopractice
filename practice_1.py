c6ba289617f320ff75cc9ab0fe6dc461c32e31d2
wudong <wu1065104859@gmail.com>
2018-03-03 20:26:01 +0800

    first practice

---
+++ practice_1.py
@@ -0,0 +1,7 @@
+#根据父母身高由遗传公式推算出自子女的身高
+
+Fa_height = int(input('Please input your father\'s height\n'))
+Ma_height = int(input('how about your mother\'s\n'))
+boy = (Fa_height+Ma_height)/2*1.08
+girl = (Fa_height*0.923+Ma_height)/2
+print('boy will be %d\ngirl will be %d' % (boy,girl))
