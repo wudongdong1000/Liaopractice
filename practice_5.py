1dc3ca98a299acbc640ee1d4039b77b47410b623
wudong <wu1065104859@gmail.com>
2018-03-04 17:33:36 +0800

    求乘积

---
+++ practice_5.py
@@ -0,0 +1,12 @@
+#计算一个若干个数的积
+def product(*numbers):
+    if not numbers:
+        
+        return '请输入值'
+    pro=1
+    for i in numbers:
+        pro=pro*i
+    
+    return pro
+print(product(*map(int,input('请输入数值，用空格隔开').split())))
+
