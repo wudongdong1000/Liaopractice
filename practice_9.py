00ad57acb2503b6b58103985571e53ecb3929864
wudong <wu1065104859@gmail.com>
2018-03-07 15:19:28 +0800

    first

---
+++ practice_9.py
@@ -0,0 +1,12 @@
+#请使用迭代查找一个list中的最小值和最大值，并返回一个tuple
+def findMinAndMax(s):
+    min=0
+    max=0
+    for i in s:
+        if i>max:
+            max=i
+    for i in s:
+        if i<min:
+            min=i
+    return max,min
+print(findMinAndMax([2,3,-1,9,19,2]))
